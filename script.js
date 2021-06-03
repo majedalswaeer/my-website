685d4b8434acd775044c1d492a5c8aa78353ebc0
majedalswaeer1 <>
2021-06-03 11:50:35 +0000

    lab06

---
+++ script.js
@@ -0,0 +1,15 @@
+var CEO = "majed";
+console.log(CEO);
+document.write("<p style= 'color:green;'> CEO is an unknown lie unless you can believe him ALSO " + CEO + " IS UNREAL because if u didnt believe the last words majed will be real, Welcome to the place that dosent exist, ENJOY OUR FAKE WEBSITE WHICH SALES everything real :) </p>")
+
+alert("THIS WEBSITE BELONGS TO NONE, UNLESS YOU CAN ENTER IT")
+
+majed=0;
+if (CEO == majed) {
+  alert("Good for u")
+} else {
+
+  alert("Great for me")
+}
+
+prompt("Please enter my name", "majed, already did :) ");
