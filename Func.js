f26d077f3802242a6286dfcf0b34ffd13e51b999
majedalswaeer <majedalswaeer69@gmail.com>
2021-06-21 12:44:58 +0300

    second

--- Func.js
+++ Func.js
@@ -1,22 +1,22 @@
-// way onefunction decleration
-function add(x,y){
- var result= x+y;
- return result;
-}
-var result2 = add(3,5));
-document.write("<p>"+ result2 + "</p>")
+// // way onefunction decleration
+// function add(x,y){
+//  var result= x+y;
+//  return result;
+// }
+// var result2 = add(3,5));
+// document.write("<p>"+ result2 + "</p>")
 
-//way2 function expression
-var sub = function(x,y){
-  var result = x-y;
-  return result
-}
-//console.log(sub(9,3))
+// //way2 function expression
+// var sub = function(x,y){
+//   var result = x-y;
+//   return result
+// }
+// //console.log(sub(9,3))
 
-var firstNum = 10;
-var secondNum = 5 ;
+// var firstNum = 10;
+// var secondNum = 5 ;
 
-var addOutput= add(firstNum,secondNum);
-var subOutput = sub(firstNum,secondNum);
-console.log(addOutput,subOutput);
+// var addOutput= add(firstNum,secondNum);
+// var subOutput = sub(firstNum,secondNum);
+// console.log(addOutput,subOutput);
 
--- script.js
+++ script.js
@@ -1,71 +1,122 @@
-// var CEO = "majed";
+// ***HERE ARE THE NOTES FOR CODE 101 & 102, IT ENDS AT LINE 62***
+//var CEO = "majed";
 // console.log(CEO);
 // document.write("<p style= 'color:green;'> CEO is an unknown lie unless you can believe him ALSO " + CEO + " IS UNREAL because if u didnt believe the last words majed will be real, Welcome to the place that dosent exist, ENJOY OUR FAKE WEBSITE WHICH SALES everything real :) </p>")
 
 // alert("THIS WEBSITE BELONGS TO NONE, UNLESS YOU CAN ENTER IT")
-///
-//majed=0;
-//if (CEO == majed) {
-///alert("Good for u")
-//} else {
+// /
+// majed=0;
+// if (CEO == majed) {
+// /alert("Good for u")
+// } else {
 
-//alert("Great for me")
-//}
+// alert("Great for me")
+// }
 
-//prompt("Please enter my name", "majed, already did :) ");
+// prompt("Please enter my name", "majed, already did :) ");
 
 // lec6
 // logical operators
 // and, or , not
-//if (CEO<=25 || CEO=25)
+// if (CEO<=25 || CEO=25)
 // || MEANS or
 // loops
-//if (var i=1;i<5;i++) {
-//console.log(i);
-//}
-///
-//while (plant != "Jade Plant" && plant != "Aloe Vera" && plant != "Passion Flower") {
+// if (var i=1;i<5;i++) {
+// console.log(i);
+// }
+// /
+// while (plant != "Jade Plant" && plant != "Aloe Vera" && plant != "Passion Flower") {
 
-/// plant = prompt("please enter on of : Jade Plant, Aloe Vera, Passion Flower");
-//}
-//document.write("<img src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
-//____________________________________________________
+// / plant = prompt("please enter on of : Jade Plant, Aloe Vera, Passion Flower");
+// }
+// document.write("<img src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
+//_______________________________________________________________________________________________________________________
 
-//var plant = prompt("whats your fav plant, choose one and get a discount ? : Jade Plant, Aloe Vera, Passion Flower ");
+// var plant = prompt("whats your fav plant, choose one and get a discount ? : Jade Plant, Aloe Vera, Passion Flower ");
 
-//for (var i=0;i<5;){
-//document.write("<img //src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
-//}
-//x=5;
-//if (plant=="Jade Plant" || plant=="Aloe Vera" || plant=="Passion Flower"){
-//console.log(plant:);
-//} else {
+// for (var i=0;i<5;){
+// document.write("<img //src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
+// }
+// x=5;
+// if (plant=="Jade Plant" || plant=="Aloe Vera" || plant=="Passion Flower"){
+// console.log(plant);
+// } else {
 //  plant = prompt("please enter one of these options:Jade Plant, Aloe Vera, Passion Flower ");
-// console.log(plant:)
-//}
-//____________________________________________________
+// console.log(plant)
+// }
+//_______________________________________________________________________________________________________________________
+// let plant = prompt("please enter on of : Jade Plant, Aloe Vera, Passion Flower", "PRESS OK AND GET YOUR OFFER :)");
 
-var plant = prompt("please enter on of : Jade Plant, Aloe Vera, Passion Flower", "PRESS OK AND GET YOUR OFFER :)");
+// function plantfunc() {
+//   for (let i = 0; i<5 ; i++) {
+//     document.write("<img src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
+//   }
+//   let plant = prompt("whats your fav plant, choose one and get a discounts ? : Jade Plant, Aloe Vera, Passion Flower ");
 
-function plantfunc() {
-  for (var i = 0; i<5 ; i++) {
-    document.write("<img src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
-  }
-
-
-  var plant = prompt("whats your fav plant, choose one and get a discounts ? : Jade Plant, Aloe Vera, Passion Flower ");
-
-  while (plant != "Jade Plant" && plant != "Aloe Vera" && plant != "Passion Flower") {
-    plant = prompt("please enter one of these options:Jade Plant, Aloe Vera, Passion Flower ");
-  }
-  document.write("<img src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
+//   while (plant != "Jade Plant" && plant != "Aloe Vera" && plant != "Passion Flower") {
+//     plant = prompt("please enter one of these options:Jade Plant, Aloe Vera, Passion Flower ");
+//   }
+//   document.write("<img src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
+// }
+// plantfunc()
+//_______________________________________________________________________________________________________________________
+// ***HERE ARE THE NOTES FOR CODE 201***
+// READ01 20-6-2021
+'use strict'; 
+let factory1 = window.prompt('which factory did you common visted ? Majed\'s factory ');
+console.log(factory1);
+let like = prompt('which type of plants you like?');
+console.log(like);
+alert("GREAT, NOW YOU WILL HAVE A DISCOUNT")
+document.write("<img src='https://www.mssdefence.com/wp-content/uploads/2016/11/Discount-Action-Mss-Defence.png'/>")
+let plant1 = prompt('whats your fav plant is it Jade Plant or Aloe Vera or Passion Flower ?!');
+console.log(plant1);
+if (plant1 == "Jade Plant" || plant1 == "Aloe Vera" || plant1 == "Passion Flower") {
+    console.log("CONGRATS YOU DESERVE 50 PERCENT DISCOUNT");
 }
-plantfunc()
+else {
+    alert("WELCOME")
+}
+//_______________________________________________________________________________________________________________________
+//DATE: 21-6-2021
+// 'use strict';
+// let num1 = parseInt (prompt("enter the first num"));
+// let num2 = parseInt (prompt("enter the second num"))
+// console.log('sum = ', num1 + num2);
+//_______________________________________________________________________________________________________________________
 
-
-
-
-
-
+// let num1 = parseInt(prompt("enter the first num"));
+// switch is similar to if statment
+// switch (num1) {
+//     case 3:
+//         console.log('correct');
+//         break;
+//     case 2:
+//         console.log('correct');
+//         break;
+//     case 5:
+//         console.log('wrong');
+//         break;
+// }
+//_______________________________________________________________________________________________________________________
+// let usertype = prompt('which user\'s type ?');
+// switch (usertype) {
+//     case 'user':
+//         console.log('srry you can\'t enter')
+//         break;
+//     case 'admin':
+//         console.log(' welcome')
+//         break;
+// }
+//_______________________________________________________________________________________________________________________
+// let cartype = prompt('which your car\'s type ?');
+// switch (cartype.toUpperCase) {
+//     case 'KIA':
+//         console.log('BAD CAR')
+//         break;
+//     case 'admin':
+//         console.log(' welcome')
+//         break;
+// }
 
 
